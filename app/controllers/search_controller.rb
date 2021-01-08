e39d813f7c6330e8efb91bdf5cf74c1b7d3792ec
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-08 11:27:27 -0600

    Update search/index action and view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # conn = Faraday.new(
-    #   url: 'https://poetrydb.org/',
-    #   headers: {'Content-Type' => 'application/json'}
-    # )
-
-    # response = conn.get("author/#{params[:author]}")
-
-    # @poems = JSON.parse(response.body, symbolize_names: true)[0..9]
-    
     @poems = SearchFacade.poems(params[:author])
   end
 end
